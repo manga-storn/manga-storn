92466092c51d6810d78dc195b9755706108825f0
Victor Belo <victorhugocamposbelo@gmail.com>
2022-10-20 16:56:22 -0300

    add taillwind css

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,17 @@
 # General application configuration
 import Config
 
+config :tailwind,
+  version: "3.1.6",
+  default: [
+    args: ~w(
+      --config=tailwind.config.js
+      --input=css/app.css
+      --output=../priv/static/assets/app.css
+    ),
+    cd: Path.expand("../assets", __DIR__)
+  ]
+
 config :manga_storn,
   ecto_repos: [MangaStorn.Repo]
 
--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :manga_storn, MangaStornWeb.Endpoint,
   secret_key_base: "zABCUsNpqj8cbg/5a/FnNdxVW6VvYdoG1KAlP84KAs5Yg19dso1hzc1cIB1S74ay",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
